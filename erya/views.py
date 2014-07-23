48c78f3b036d05fe5c23096e85cf5fe34f6c7d8d
Perchouli <jp.chenyang@gmail.com>
2014-07-23 21:38:51 +0800

    FIX: bug with order

--- erya/views.py
+++ erya/views.py
@@ -5,9 +5,8 @@ from django.template.response import TemplateResponse
 from posts.models import Post, Reply
 
 def home(request):
-    posts = Post.objects.all().order_by('-date')
 
     ctx = {
-        'recent_replies' : Reply.objects.all()[:10],
+        'recent_replies' : Reply.objects.all().order_by('-created_at')[:10],
     }
     return TemplateResponse(request, 'home.html', ctx)
