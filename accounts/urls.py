ceec9492b2aaa79842884f56fcc7496c4b2e7fd2
Perchouli <jp.chenyang@gmail.com>
2015-04-30 15:13:33 +0800

    urls.py remove string view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 
 from .forms import LoginForm
+from . import views
 
-urlpatterns = patterns('accounts.views',
+urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html', 'authentication_form': LoginForm}, name='login'),
-    url(r'^register/$', 'register', name='register'),
+    url(r'^register/$', views.register, name='register'),
     # url(r'^settings/$', 'settings', name='settings'),
-    url(r'^profile/(?P<user_id>\d+)/$', 'profile', name='profile'),
+    url(r'^profile/(?P<user_id>\d+)/$', views.profile, name='profile'),
     # url(r'^password/set/$','set_password'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'home.html'}, name='logout'),
-)
+]
--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('posts.views',
-    url(r'^category/(?P<category_id>\d+)/$', 'post_list', name='category'),
-    url(r'^(?P<post_id>\d+)/$', 'post_detail', name='post_detail'),
-    url(r'^create/$', 'create'),
-    url(r'^delete/$', 'delete'),
-    url(r'^reply/(?P<post_id>\d+)/$', 'reply', name='reply_post'),
-    url(r'^upload/$', 'upload'),
-    url(r'^edit/(?P<post_id>\d+)/$', 'edit', name='edit_post'),
-)
+from . import views
+
+urlpatterns = [
+    url(r'^category/(?P<category_id>\d+)/$', views.post_list, name='category'),
+    url(r'^(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^create/$', views.create),
+    url(r'^delete/$', views.delete),
+    url(r'^reply/(?P<post_id>\d+)/$', views.reply, name='reply_post'),
+    url(r'^upload/$', views.upload),
+    url(r'^edit/(?P<post_id>\d+)/$', views.edit, name='edit_post'),
+]
