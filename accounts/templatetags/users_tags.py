c6cbccca11b9fc5c778af6dba90c00c46f375911
Perchouli <jp.chenyang@gmail.com>
2015-09-23 15:57:58 +0800

    Add divider before segment

--- accounts/templatetags/users_tags.py
+++ accounts/templatetags/users_tags.py
@@ -8,4 +8,4 @@ register = template.Library()
 
 @register.filter
 def gravatar(value):
-    return 'http://gravatar.duoshuo.com/avatar/' + hashlib.md5(value.encode('utf-8').lower()).hexdigest()
+    return 'http://cn.gravatar.com/avatar/' + hashlib.md5(value.encode('utf-8').lower()).hexdigest()
