7be14c85da9ed73b886c3328b61749a3f6d9c595
Perchouli <jp.chenyang@gmail.com>
2016-09-21 23:30:56 +0800

    Use ui container

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^category/(?P<category_id>\d+)/$', views.post_list, name='category'),
     url(r'^(?P<post_id>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^create/$', views.create),
     url(r'^delete/$', views.delete),
