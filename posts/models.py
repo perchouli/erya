eb2ea245afe8534bb72fa046778c62d3c6ac65aa
Perchouli <jp.chenyang@gmail.com>
2016-09-27 22:45:33 +0800

    Add parent

--- posts/models.py
+++ posts/models.py
@@ -62,6 +62,7 @@ class Post(models.Model):
     reply = models.DateTimeField(auto_now_add=True)
     pageviews = models.IntegerField(default=0)
     comments = models.IntegerField(default=0)
+    parent = models.ForeignKey('self', null=True)
 
     focus = models.BooleanField(default=False)
 
